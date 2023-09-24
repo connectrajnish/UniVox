1361f41f7f920f1982f8171ebf9b478a2573e602
connectrajnish <rajnishresonite@gmail.com>
2023-09-25 05:02:59 +0530

    eastablished client side routing using react router dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Home from "./Home";
+import ErrorPage from "./ErrorPage";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <div className=" bg-gray-100 fixed flex w-[100vw] h-auto top-0 border-b-4">
-              <Navbar />
-            </div> 
+        <Navbar />
+      </div>
+      <RouterProvider router={router}>
+        <div>
+          {/* Outlet will be filled dynamically i.e. conditional routing*/}
+          <Outlet />
+        </div>
+      </RouterProvider>
     </div>
   );
 };
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 export default App;
