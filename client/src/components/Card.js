7d29f821550896af3f907a79a4ae7b9552c983dd
connectrajnish <rajnishresonite@gmail.com>
2023-10-01 02:58:09 +0530

    Dialog for view more

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Card,
   CardHeader,
@@ -7,29 +8,18 @@ import {
   Button,
 } from "@material-tailwind/react";
 
-// accept props like heading, content etc
-export default function Card() {
-  const title = "Atlassian Interview Experience for SDE";
-  const content = `The Atlassian Software Development Engineer (SDE) interview process comprised four rounds.
-    The initial round included a Hackerrank coding assessment with three coding questions,
-    leading to the selection of 20 candidates out of 317. The subsequent rounds involved
-    technical assessments, system design discussions, and a values and management interview.
-    In the end, five students, including the interviewee, were successfully chosen as part
-    of Atlassian's rigorous evaluation process, encompassing both technical proficiency and
-    alignment with the company's values.`;
+import DialogCustomAnimation from "./PostDialog";
 
-  // Find the last space character within the first 100 characters
-  const lastSpaceIndex = content.lastIndexOf(" ", 100);
+export default function CardComponent() {
+  const title = "Atlassian Interview Experience for SDE";
+  const content = `The Atlassian Software Development Engineer (SDE) interview process comprised four rounds. The initial round included a Hackerrank coding assessment with three coding questions, leading to the selection of 20 candidates out of 317. The subsequent rounds involved technical assessments, system design discussions, and a values and management interview. In the end, five students, including the interviewee, were successfully chosen as part of Atlassian's rigorous evaluation process, encompassing both technical proficiency and alignment with the company's values.`;
 
   // Truncate the content at the last space
-  let truncatedContent =
-    lastSpaceIndex === -1 ? content : content.substring(0, lastSpaceIndex);
+  let truncatedContent = content.length > 100 ? content.substring(0, 100) + "..." : content;
 
-  // Add three dots if content is truncated
-  truncatedContent =
-    content.length > truncatedContent.length
-      ? truncatedContent + "..."
-      : truncatedContent;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
 
   return (
     <Card className="mt-0 min-w-full">
@@ -40,7 +30,17 @@ export default function Card() {
         <Typography>{truncatedContent}</Typography>
       </CardBody>
       <CardFooter className="pt-0">
-        {content.length > 100 && <Button>View More</Button>}
+        {content.length > 100 && (
+          <>
+            <Button onClick={handleOpen}>View More</Button>
+            <DialogCustomAnimation
+              open={open}
+              handleOpen={handleOpen}
+              title={title}
+              content={content}
+            />
+          </>
+        )}
       </CardFooter>
     </Card>
   );
---
+++ client/src/components/PostDialog.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  IconButton,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+
+export default function DialogCustomAnimation({
+  open,
+  handleOpen,
+  title,
+  content,
+}) {
+  return (
+    <Dialog
+      open={open}
+      handler={handleOpen}
+      animate={{
+        mount: { scale: 1, y: 0 },
+        unmount: { scale: 0.9, y: -100 },
+      }}
+    >
+      <DialogHeader className="justify-between">
+        {title}
+        <IconButton
+          color="red"
+          size="sm"
+          variant="text"
+          onClick={handleOpen}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+            className="h-5 w-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </IconButton>
+      </DialogHeader>
+      <DialogBody divider>{content}</DialogBody>
+      <DialogFooter></DialogFooter>
+    </Dialog>
+  );
+}
