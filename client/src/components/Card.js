b6e508d71d2425a5ae139c4090178b6d3ac3a526
connectrajnish <rajnishresonite@gmail.com>
2023-10-01 01:23:11 +0530

    Home layout completed and hide scrolling in home layout

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,28 +1,47 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-  } from "@material-tailwind/react";
-   
-  export default function CardDefault() {
-    return (
-      <Card className="mt-6 w-96">
-        <CardBody>
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            UI/UX Review Check
-          </Typography>
-          <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-            night life in Barcelona.
-          </Typography>
-        </CardBody>
-        <CardFooter className="pt-0">
-          <Button>Read More</Button>
-        </CardFooter>
-      </Card>
-    );
-  }
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+
+// accept props like heading, content etc
+export default function Card() {
+  const title = "Atlassian Interview Experience for SDE";
+  const content = `The Atlassian Software Development Engineer (SDE) interview process comprised four rounds.
+    The initial round included a Hackerrank coding assessment with three coding questions,
+    leading to the selection of 20 candidates out of 317. The subsequent rounds involved
+    technical assessments, system design discussions, and a values and management interview.
+    In the end, five students, including the interviewee, were successfully chosen as part
+    of Atlassian's rigorous evaluation process, encompassing both technical proficiency and
+    alignment with the company's values.`;
+
+  // Find the last space character within the first 100 characters
+  const lastSpaceIndex = content.lastIndexOf(" ", 100);
+
+  // Truncate the content at the last space
+  let truncatedContent =
+    lastSpaceIndex === -1 ? content : content.substring(0, lastSpaceIndex);
+
+  // Add three dots if content is truncated
+  truncatedContent =
+    content.length > truncatedContent.length
+      ? truncatedContent + "..."
+      : truncatedContent;
+
+  return (
+    <Card className="mt-0 min-w-full">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {title}
+        </Typography>
+        <Typography>{truncatedContent}</Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        {content.length > 100 && <Button>View More</Button>}
+      </CardFooter>
+    </Card>
+  );
+}
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,17 +7,17 @@ const Home = () => {
   return (
     <div className="flex flex-col md:flex-row">
       {/* Left Sidebar */}
-      <div className="md:w-1/5 bg-gray-200 rounded-lg max-h-screen overflow-y-auto">
+      <div className="md:w-1/5 bg-gray-200 rounded-lg max-h-screen overflow-y-auto no-scrollbar">
         <LeftSidebar />
       </div>
 
       {/* Main Content */}
-      <div className="md:w-3/5 bg-white p-4 max-h-screen overflow-y-auto">
+      <div className="md:w-3/5 bg-white p-4 max-h-screen overflow-y-auto no-scrollbar">
         <Content />
       </div>
 
       {/* Right Sidebar */}
-      <div className="md:w-1/5 bg-gray-200 rounded-lg max-h-screen overflow-y-auto">
+      <div className="md:w-1/5 bg-gray-200 rounded-lg max-h-screen overflow-y-auto no-scrollbar">
         <RightSideBar />
       </div>
     </div>
