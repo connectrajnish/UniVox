f931106c5affd442036ad5367a937a39f87d31d4
connectrajnish <rajnishresonite@gmail.com>
2023-10-04 03:45:47 +0530

    upvote feature

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -61,6 +61,12 @@ export default function CardComponent() {
 
   const handleOpen = () => setOpen(!open);
 
+  const [upvotes, setUpvotes] = useState(0);
+  const handleUpvote = () => {
+    // Increment the upvote count
+    upvotes ? setUpvotes(upvotes - 1) : setUpvotes(upvotes + 1);
+  };
+
   return (
     <Card className="mt-0 min-w-full">
       <CardHeader
@@ -79,7 +85,44 @@ export default function CardComponent() {
         </Typography>
         <Typography>{truncatedContent}</Typography>
       </CardBody>
-      <CardFooter className="pt-0">
+      <CardFooter className="pt-0 flex justify-between">
+        
+          <Button variant="text" onClick={handleUpvote} className="flex items-center justify-center" >
+            {upvotes === 0 ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                {/* Upvote SVG icon */}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                {/* Downvote SVG icon */}
+                <path
+                  fillRule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm.53 5.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72v5.69a.75.75 0 001.5 0v-5.69l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+            &nbsp;
+            {upvotes}
+          </Button>
+        
         {content.length > 100 && (
           <>
             <Button onClick={handleOpen}>View More</Button>
